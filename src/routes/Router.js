19719d309c51c8bb75281b3032531bda281f0f5f
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-07-18 17:36:03 -0300

    Adicionando rotas

---
+++ src/routes/Router.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+import DetailsPage from '../pages/DetailsPage/DetailsPage'
+import Pokedex from '../pages/Pokedex/Pokedex'
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />}/>
+            <Route path='details/:id' element={<DetailsPage />} />
+            <Route path='pokedex' element={<Pokedex />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
