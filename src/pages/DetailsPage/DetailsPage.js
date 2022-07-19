6fde2b5f86b3fb150d77e3fee9c7f34245a096a0
Joisantos <joice.pakua@gmail.com>
2022-07-19 17:54:35 -0300

    estilo detalhes

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goToHome } from '../../routes/Coordinator'
+import Header from '../../components/Header/Header'
+import { MainDiv } from '../../components/Header/Styled'
+import { backDetails, pngWing, pokemon, spriteFront, spriteBack, stats, statsImg, moves } from './Styled'
 
 export default function DetailsPage() {
   const navigate = useNavigate()
 
   return (
-    <div> 
+    <MainDiv>
+      <Header />
       <h1>DetailsPage</h1>
+      <button onClick={() => goToHome(navigate)}>Todos os Pokémons</button>
+      <backDetails>
+        <spriteFront>'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'</spriteFront>
+        <spriteBack>''</spriteBack>
+        <stats>
+          <statsImg>''</statsImg>
+        </stats>
+        <pokemon>'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'</pokemon>
+        <moves>'</moves>
+      </backDetails>
 
-    <button onClick={() => goToHome(navigate)}>Todos os Pokémons</button>
-    </div>
+    </MainDiv>
   );
 };
---
+++ src/pages/DetailsPage/Styled.js
@@ -0,0 +1,88 @@
+import styled from "styled-components"
+
+export const backDetails = styled.div `
+position: absolute;
+width: 1389.14px;
+height: 663px;
+left: 0px;
+top: 0px;
+background: #729F92; 
+border-radius: 37.8857px;
+`
+
+export const pngWing = styled.img `
+position: absolute;
+width: 665.31px;
+height: 665.31px;
+left: 944.66px;
+top: -122px;
+
+background: url(pngwing.png);
+opacity: 0.17;
+transform: rotate(30deg);
+`
+
+export const pokemon = styled.img `
+position: absolute;
+width: 270px;
+height: 270px;
+left: 1109px;
+top: 216px;
+
+background: url(image.png);
+`
+
+export const spriteFront = styled.img `
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 26px;
+
+background: url(image.png), #FFFFFF;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+`
+
+export const spriteBack = styled.img `
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 355px;
+
+background: url(image.png), #FFFFFF;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+`
+export const stats = styled.div `
+position: absolute;
+width: 343px;
+height: 613px;
+left: 360px;
+top: 24px;
+
+background: #FFFFFF;
+border-radius: 12px;
+`
+
+export const statsImg = styled.img `
+position: absolute;
+width: 307px;
+height: 257px;
+left: 378px;
+top: 43px;
+
+background: url(image.png);
+`
+
+export const moves = styled.div `
+position: absolute;
+width: 292px;
+height: 453px;
+left: 771px;
+top: 184px;
+
+background: #FFFFFF;
+border-radius: 8px;
+`
