b58cc6bded4122915901f07869d2b677fce40abd
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-07-21 15:50:26 -0300

    Adicionando atualização 2

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
 import { goToHome } from '../../routes/Coordinator'
 import Header from '../../components/Header/Header'
 import { BackDetails, PngWing, Pokemon, SpriteFront, SpriteBack, Stats, StatsImg, Moves } from './Styled'
 import { GlobalContext } from '../../global/GlobalContext'
 import { BaseUrl } from '../../contants/urls'
+import useRequestData from '../../hooks/useRequestData'
+
+
 
 export default function DetailsPage() {
   const navigate = useNavigate()
 
   const { pokemons, setPokemons } = useContext(GlobalContext)
+  const detailList = useRequestData(`${BaseUrl}/1/`)
+  const abilities = detailList.abilities
+  console.log(abilities)
+  
+  const chooseScreen = () => {
+    // switch (pokemonsList) {
+    //   case undefined:
+    //     return (
+    //       <h1>Carregando</h1>
+    //     )
+    //   case []:
+    //     return (
+    //       <h1>Carregando</h1>
+    //     )
+    //   default:
+    //     return (
+    //       pokemonsList.map((pokemon) => {
+    //         return (
+    //           <>
+    //             <SpriteFront alt='SpriteFront' src={pokemon.sprites.front_default}></SpriteFront>
+    //             <SpriteBack alt='SpriteBack' src={pokemon.sprites.back_default}></SpriteBack>
+    //             <Stats>
+    //               <p>Base stats</p>
+    //               <StatsImg alt='StatsImg' src={pokemon.stats} ></StatsImg>
+    //             </Stats>
+    //             <Pokemon alt='Pokemon' src={pokemon.sprites.front_default}></Pokemon>
+    //             <Moves>{`${BaseUrl}/moves`}</Moves>
+    //           </>
+    //         )
+    //       }
+    //       )
+    //     )
+    // }
+  }
 
   return (
-    <MainDiv>
+    <div>
       <Header />
       <h1>DetailsPage</h1>
       <button onClick={() => goToHome(navigate)}>Todos os Pokémons</button>
-      <BackDetails>  
-        {pokemons.map((pokemon) => {
-          return (
-            <>
-              <SpriteFront alt='SpriteFront' src={pokemon.sprites.front_default}></SpriteFront>
-              <SpriteBack alt='SpriteBack' src={pokemon.sprites.back_default}></SpriteBack>
-              <Stats>
-                <p>Base stats</p>
-                <StatsImg alt='StatsImg' src={pokemon.stats} ></StatsImg>
-              </Stats>
-              <Pokemon alt='Pokemon' src={pokemon.sprites.front_default}></Pokemon>
-              <Moves>{`${BaseUrl}/moves`}</Moves>
-            </>
-          )
-        }
-        )
-        };
+      <BackDetails>
+        {chooseScreen()}
       </BackDetails>
-    </MainDiv>
+
+    </div>
   );
 };
