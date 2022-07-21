91770ff7eb002811c104e1408ddcfcf8aab8bae8
Joisantos <joice.pakua@gmail.com>
2022-07-21 20:54:29 -0300

    codigo pagina detalhes

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { goToHome } from '../../routes/Coordinator'
 import Header from '../../components/Header/Header'
-import { BackDetails, PngWing, Pokemon, SpriteFront, SpriteBack, Stats, StatsImg, Moves } from './Styled'
+import { BackDetails, Pokemon, SpriteFront, SpriteBack, Stats, StatsImg, Moves } from './Styled'
 import { GlobalContext } from '../../global/GlobalContext'
 import { BaseUrl } from '../../contants/urls'
 import useRequestData from '../../hooks/useRequestData'
@@ -14,39 +14,40 @@ export default function DetailsPage() {
   const navigate = useNavigate()
 
   const { pokemons, setPokemons, idDetails } = useContext(GlobalContext)
+  const [pokemonsList, setpokemonsList] = useState([])
   const detailList = useRequestData(`${BaseUrl}/${idDetails}/`)
   console.log(detailList) 
-  // const abilities = detailList.abilities
+ //const abilities = detailList.abilities
   
   const chooseScreen = () => {
-    // switch (pokemonsList) {
-    //   case undefined:
-    //     return (
-    //       <h1>Carregando</h1>
-    //     )
-    //   case []:
-    //     return (
-    //       <h1>Carregando</h1>
-    //     )
-    //   default:
-    //     return (
-    //       pokemonsList.map((pokemon) => {
-    //         return (
-    //           <>
-    //             <SpriteFront alt='SpriteFront' src={pokemon.sprites.front_default}></SpriteFront>
-    //             <SpriteBack alt='SpriteBack' src={pokemon.sprites.back_default}></SpriteBack>
-    //             <Stats>
-    //               <p>Base stats</p>
-    //               <StatsImg alt='StatsImg' src={pokemon.stats} ></StatsImg>
-    //             </Stats>
-    //             <Pokemon alt='Pokemon' src={pokemon.sprites.front_default}></Pokemon>
-    //             <Moves>{`${BaseUrl}/moves`}</Moves>
-    //           </>
-    //         )
-    //       }
-    //       )
-    //     )
-    // }
+/*   switch (detailList) {
+     case undefined:
+       return (
+         <h1>Carregando</h1>
+       )
+     case []:
+       return (
+         <h1>Carregando</h1>
+       )
+     default:
+       return (
+          detailList.map((poke) => {
+           return (
+             <>
+               <SpriteFront alt='SpriteFront' src={poke.sprites.front_default}></SpriteFront>
+               <SpriteBack alt='SpriteBack' src={poke.sprites.back_default}></SpriteBack>
+                <Stats>
+                 <p>Base stats</p>
+                 <StatsImg alt='StatsImg' src={poke.stats} ></StatsImg>
+               </Stats>
+                <Pokemon alt='Pokemon' src={poke.sprites.front_default}></Pokemon>
+               <Moves>{`${BaseUrl}/moves`}</Moves>
+             </>
+           )
+          }
+         )
+       )
+   } */
   }
 
   return (
